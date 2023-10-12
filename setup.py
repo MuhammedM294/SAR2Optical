d3ffea43039be18a7d3ab460510bbaa2105a2e03
MuhammedM294 <muhammedaabdelaal@gmail.com>
2023-10-12 21:53:34 +0200

    init commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This repository hosts a collection of experiments aimed at testing the effectiveness of transcoding Synthetic Aperture Radar (SAR) imagery to optical ones. The focus of these experiments is on solving the challenge posed by the similarity of waterbodies and sand landcover in SAR images, particularly in desert areas.',
+    author='Muhammed Abdelaal',
+    license='MIT',
+)
