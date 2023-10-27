6ecac5effb289b2bf1a013a6c2f5efa7f3989236
MuhammedM294 <muhammedaabdelaal@gmail.com>
2023-10-27 10:10:19 +0200

    init models

--- src/models/model.py
+++ src/models/model.py
@@ -118,5 +118,93 @@ class Discriminator(nn.Module):
         return self.model(img_input)
     
 
+class Generator(nn.Module):
+    """
+    Define the generator architecture
+    """
+    def __init__(self, input_c, output_c, use_dropout, use_bias):
+        super(Generator, self).__init__()
+        self.model = nn.Sequential(
+            # First fixed layer
+            nn.ReflectionPad2d(3),
+            nn.Conv2d(input_c, 64, 7, bias=use_bias),
+            nn.InstanceNorm2d(64),
+            nn.ReLU(inplace=True),
+
+            # Downsampling fixed at 2
+            nn.Conv2d(64, 128, 3, stride=2, padding=1, bias=use_bias),
+            nn.InstanceNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 256, 3, stride=2, padding=1, bias=use_bias),
+            nn.InstanceNorm2d(256),
+            nn.ReLU(inplace=True),
+            #nn.Conv2d(256, 512, 3, stride=2, padding=1, bias=use_bias),
+            #nn.InstanceNorm2d(512),
+            #nn.ReLU(inplace=True),
+
+            # Residual blocks
+            ResidualBlock(256, use_dropout, use_bias),
+            ResidualBlock(256, use_dropout, use_bias),
+            ResidualBlock(256, use_dropout, use_bias),
+
+            ResidualBlock(256, use_dropout, use_bias),
+            ResidualBlock(256, use_dropout, use_bias),
+            ResidualBlock(256, use_dropout, use_bias),
+
+            ResidualBlock(256, use_dropout, use_bias),
+            ResidualBlock(256, use_dropout, use_bias),
+            ResidualBlock(256, use_dropout, use_bias),
+
+            # Upsampling fixed at 2
+            #nn.ConvTranspose2d(512, 256, 3, stride=2, padding=1, output_padding=1, bias=use_bias),
+            #nn.InstanceNorm2d(256),
+            #nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(256, 128, 3, stride=2, padding=1, output_padding=1, bias=use_bias),
+            nn.InstanceNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(128, 64, 3, stride=2, padding=1, output_padding=1, bias=use_bias),
+            nn.InstanceNorm2d(64),
+            nn.ReLU(inplace=True),
+
+            # Last layer fixed
+            nn.ReflectionPad2d(3),
+            nn.Conv2d(64, output_c, 7),
+            #nn.Tanh(),
+            nn.Conv2d(output_c, output_c, 1),
+            #nn.Conv2d(output_c, output_c, 1),
+        )
+
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class ResidualBlock(nn.Module):
+    """
+    Residual block definition
+    """
+    def __init__(self, in_channels, use_dropout, use_bias):
+        super(ResidualBlock, self).__init__()
+
+        if use_dropout:
+            self.res = nn.Sequential(nn.ReflectionPad2d(1),
+                                     nn.Conv2d(in_channels, in_channels, 3, bias=use_bias),
+                                     nn.InstanceNorm2d(in_channels),
+                                     nn.ReLU(inplace=True),
+                                     nn.Dropout(0.5),
+                                     nn.ReflectionPad2d(1),
+                                     nn.Conv2d(in_channels, in_channels, 3, bias=use_bias),
+                                     nn.InstanceNorm2d(in_channels))
+        else:
+            self.res = nn.Sequential(nn.ReflectionPad2d(1),
+                                     nn.Conv2d(in_channels, in_channels, 3, bias=use_bias),
+                                     nn.InstanceNorm2d(in_channels),
+                                     nn.ReLU(inplace=True),
+                                     nn.ReflectionPad2d(1),
+                                     nn.Conv2d(in_channels, in_channels, 3, bias=use_bias),
+                                     nn.InstanceNorm2d(in_channels))
+
+    def forward(self, x):
+        return x + self.res(x)
 
 
